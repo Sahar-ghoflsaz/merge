d3d5b0d2386e79e292927e686d8663f856a93d90
Sahar-ghoflsaz <64737449+Sahar-ghoflsaz@users.noreply.github.com>
2020-05-21 20:43:08 +0430

    Update merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -1,71 +1,83 @@
 #include <iostream>
 using namespace std;
 
-int searchA(int *outarray, int n, int number){
-
-    for( int i=0 ; i<n ; i++){
-        if(outarray[i] == number) return i;
-    }
-    return -1;
+bool searchA(int* outarray, int n, int number)
+{
+    for (int i = 0; i < n; i++) 
+        if (outarray[i] == number) 
+            return false;
+    
+    return true;
 }
 
-int main(){
+int main() {
 
-    int n;
-    cin>> n;
-    int *FirstArray = new int [n];
+    int n = -1;
+    while (n < 0)
+    {
+        cout << "Please enter a positive number for the size of the first array: ";
+        cin >> n;
+    }
+    
+    int* FirstArray = new int[n];
 
-    for( int i=0 ; i<n ; i++){
+    cout << "\n\nEnter the first array's elemenct one by one: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> FirstArray[i];
+    }
+        
+ 
+    int m = -1;
 
-       cin >> FirstArray[i];
+    while(m < 0)
+    {
+        cout << "\nPlease enter a positive number for the size of the second array: ";
+        cin >> m;
+    }
+    
+    int* SecondArray = new int[m];
+
+
+    cout << "\n\nEnter the second array's elemenct one by one: \n";
+
+    for (int i = 0; i < m; i++) 
+    {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> SecondArray[i];
     }
 
-    int m;
-    cin>> m;
-    int *SecondArray = new int [m];
+    int* OutputArray = new int[n + m];
 
-    for( int i=0 ; i<m ; i++){
+    int counter = 0;
 
-       cin >> SecondArray[i];
-    }
-
-    int *OutputArray = new int [n+m];
-
-    for( int i=0 ; i<n+m ; i++){
-
-        OutputArray[i] = 0;
-    }
-
-    int flag=0;
-    int counter= 0;
-
-    for( int i=0 ; i<n ; i++){
-
-        flag = searchA(OutputArray, counter, FirstArray[i]);
-        if(flag == -1){
+    for (int i = 0; i < n; i++) {
+        if (searchA(OutputArray, counter, FirstArray[i]))
+        {
             OutputArray[counter] = FirstArray[i];
             counter++;
         }
     }
 
-    for( int i=0 ; i<m ; i++){
-
-        flag = searchA(OutputArray, counter, SecondArray[i]);
-        if(flag == -1){
+    for (int i = 0; i < m; i++) {
+        if (searchA(OutputArray, counter, SecondArray[i]))
+        {
             OutputArray[counter] = SecondArray[i];
             counter++;
         }
     }
 
-    for( int i=0 ; i<counter ; i++){
+    cout << "\n\nHere is the result:\n";
+    for (int i = 0; i < counter; i++) 
+        cout << OutputArray[i] << "  ";
 
-        cout<< OutputArray[i]<<"  ";
-    }
+    cout << "\n\n=========================================================================\n\n";
 
-    delete [] FirstArray;
-    delete [] SecondArray;
-    delete [] OutputArray;
+    delete[] FirstArray;
+    delete[] SecondArray;
+    delete[] OutputArray;
 
-  return 0;
+    return 0;
 
 }
